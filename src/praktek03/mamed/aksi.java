19bf8e09c1896a3d4b72f793788797d22d22d567
mamedpoo <32448088+mamedpoo@users.noreply.github.com>
2018-04-08 20:04:11 +0800

    Instance, buat atribut dan panggil method

--- src/praktek03/mamed/aksi.java
+++ src/praktek03/mamed/aksi.java
@@ -3,6 +3,11 @@ package praktek03.mamed;
 
 public class aksi {
     public static void main(String[] args) {
-        
+        baju s = new baju();
+        s.jenis_baju="Kaos";
+        s.merk="Owners";
+        s.harga=10000;
+         
+        s.cetakInfo();
     }
 }
