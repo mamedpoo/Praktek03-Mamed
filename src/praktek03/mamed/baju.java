c09139e54f53e832e9e25790f7126440807b274c
mamedpoo <32448088+mamedpoo@users.noreply.github.com>
2018-04-08 20:05:33 +0800

    set atribut private

--- src/praktek03/mamed/baju.java
+++ src/praktek03/mamed/baju.java
@@ -1,9 +1,9 @@
 
 package praktek03.mamed;
 public class baju {
-    String jenis_baju;
-    String merk;
-    double harga;
+   private String jenis_baju;
+   private String merk;
+   private double harga;
     
     void cetakInfo(){
     System.out.println("Jenis Baju \t:" + jenis_baju+"\n"+
