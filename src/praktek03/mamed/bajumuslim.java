e62b3aa541e90405a182130513e8dffafdd5ea75
mamedpoo <32448088+mamedpoo@users.noreply.github.com>
2018-04-08 20:41:32 +0800

    attribut dan method

--- src/praktek03/mamed/bajumuslim.java
+++ src/praktek03/mamed/bajumuslim.java
@@ -4,4 +4,10 @@ package praktek03.mamed;
 
 public class bajumuslim extends baju {
     
+    private String ukuran;
+     
+    void throttle(){
+        System.out.println("string gaje.......;");
+    }
+    
 }
